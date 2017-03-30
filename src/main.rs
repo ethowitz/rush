2ae980025a324f02b566446b48704bfb4993f969
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-03-30 16:17:38 -0400

    IT COMPILES

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,45 @@ use std::{env, io};
 use std::collections::HashMap;
 // mod trie;
 
+/*************************************** definitions *********************************************/
+
 enum Val {
     Num { value: i64 },
     Sym { value: String },
     Bool { value: bool },
+    Nil {},
+}
+
+impl Clone for Val {
+    fn clone(&self) -> Val {
+        match *self {
+            Val::Num { ref value } => Val::Num { value: value.clone()},
+            Val::Sym { ref value } => Val::Sym { value: value.clone()},
+            Val::Bool { ref value } => Val::Bool { value: value.clone()},
+            Val::Nil {} => Val::Nil{},
+        }
+    }
 }
 
 type Env = HashMap<String, Val>;
+const PROMPT: &'static str = ">>";
 
 enum Exp {
     Literal { value: Val },
     VarName { name: String },
-    If { e1: Box<Exp>, e2: Box<Exp>, e3: Box<Exp> },
+    //If { e1: Box<Exp>, e2: Box<Exp>, e3: Box<Exp> },
     Let { name: String, e: Box<Exp> },
-    Command { cmd: String, args: String}, // variable lookup --> command
+    //Command { cmd: String, args: String}, // variable lookup --> command
     Empty {},
 }
+
+/******************************************* parser **********************************************/
 
 fn parse_code(code: &str) -> Vec<Exp> {
     let raw_exps: Vec<&str> = code.split(';' /* | && */).collect();
     let mut exps = Vec::new();
     for e in raw_exps {
-        let words = (e as &str).split_whitespace().collect();// this needs to be smarter --> write function
+        let words = (e as &str).split_whitespace().collect();// this needs to be smarter
         exps.push(parse(&words));
     }
     exps
@@ -35,7 +52,7 @@ fn parse(words: &Vec<&str>) -> Exp {
             "let" => {
                 if words.len() != 3 {
                     println!("syntax error: \"let\" expects a name followed by an expression");
-                    return Exp::Literal {value: "error".to_string()}; // --> obviously change this
+                    return Exp::Empty {}; // --> obviously change this
                 } else {
                     return Exp::Let {
                         name: words[1].to_string(),
@@ -43,37 +60,43 @@ fn parse(words: &Vec<&str>) -> Exp {
                     };
                 }
             },
-            _ => return Exp::Literal {value: "lol".to_string()},
+            _ => return Exp::Empty {},
         }
     } else {
         Exp::Empty {}
     }
 }
 
-pub fn eval_exps(exps: Vec<Exp>, &mut env: Env) {
+/****************************************** evaluator ********************************************/
+
+fn eval_exps(exps: Vec<Exp>, env: &mut Env) {
     for e in exps {
-        eval(e, &mut env);
+        eval(e, env);
     }
 }
 
-fn eval(e: Exp, &mut env: Env) -> Val {
+fn eval(e: Exp, env: &mut Env) -> Val {
     match e {
-        Exp::Literal { name } => name,
+        Exp::Literal { value } => value,
         Exp::VarName { name } => {
-            match env.get(name) {
-                Some(value) => value,
+            match env.get(&name) {
+                Some(value) => (*value).clone(),
                 None => {
                     println!("error: variable \"{}\" not bound", name);
-                    return;
+                    Val::Nil {}
                 }
             }
         },
-        Exp::Let { name, exp } => {
-            env.insert(name, eval(exp, &mut env));
+        Exp::Let { name, e } => {
+            let v = eval(*e, env);
+            env.insert(name, v);
             Val::Bool { value: true }
         }
+        Exp::Empty {} => Val::Nil {},
     }
 }
+
+/*************************************************************************************************/
 
 fn main() {
     /* initialization
@@ -82,18 +105,17 @@ fn main() {
             + load .rushrc
             + set current directory to current user's home
     */
-    let env: Env = HashMap::new();
+    let mut env: Env = HashMap::new();
     let mut code = String::new();
 
-
     loop {
+        print!("{} ", PROMPT);
         match io::stdin().read_line(&mut code) {
             Ok(_n) => {
                 code.pop(); // remove newline
                 let exps = parse_code(&code);
                 //type::type(&exps);
-                eval_exps(&exps, &env);
-
+                eval_exps(exps, &mut env);
             }
             Err(error) => println!("error: {}", error),
         }
