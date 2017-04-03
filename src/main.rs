81ffdad35fec55eb3b259f7ec5636699d34e8e76
ethowitz <ethan.donowitz@gmail.com>
2017-04-03 19:01:58 -0400

    finished parser for basic arithmetic expressions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate regex;
 use regex::Regex;
 use std::{env, io};
 use std::collections::HashMap;
-
+use std::str::FromStr;
 
 // mod trie;
 
@@ -13,7 +13,7 @@ enum Val {
     Num { value: i64 },
     Sym { value: String },
     Bool { value: bool },
-    Nil {},
+    Nil,
 }
 
 impl Clone for Val {
@@ -22,7 +22,7 @@ impl Clone for Val {
             Val::Num { ref value } => Val::Num { value: value.clone() },
             Val::Sym { ref value } => Val::Sym { value: value.clone() },
             Val::Bool { ref value } => Val::Bool { value: value.clone() },
-            Val::Nil {} => Val::Nil{},
+            Val::Nil => Val::Nil,
         }
     }
 }
@@ -30,59 +30,207 @@ impl Clone for Val {
 type Env = HashMap<String, Val>;
 const PROMPT: &'static str = ">>";
 
+enum BinaryOp { Add, Sub, Mult, Div, Mod, Lt, Gt, Lte, Gte, Eq, Neq, And, Or }
+enum UnaryOp { Negate, Inverse }
+
 enum Exp {
     Literal { value: Val },
-    VarName { name: String },
+    //VarName { name: String },
+    Binary { operator: BinaryOp, left: Box<Exp>, right: Box<Exp>},
+    Unary { operator: UnaryOp, operand: Box<Exp> },
     //If { e1: Box<Exp>, e2: Box<Exp>, e3: Box<Exp> },
-    Let { name: String, e: Box<Exp> },
-    Command { cmd: String, args: Vec<String> }, // catch-all
-    Empty {},
+    //Let { name: String, e: Box<Exp> },
+    //Command { cmd: String, args: Vec<String> }, // catch-all
+    Empty,
 }
 
 /************************************* parser ****************************************************/
 
-fn parse_code(code: &str) -> Vec<Exp> {
-    let concrete_exps: Vec<&str> = code.split(';' /* | && */).collect();
+type Tokens<'a> = std::iter::Peekable<std::slice::Iter<'a, &'a str>>;
 
-    let mut exps = Vec::new();
-    for e in concrete_exps {
-        let mut tokens = tokenize(e);
-        exps.push(parse(tokens));
-    }
-    exps
-}
-
-fn tokenize(exp: &str) -> Vec<&str> {
+// TODO: currently this would only recognize 1 + 2 and not 1+2
+//      --> replace important every "token" with " token " to ensure split will catch
+//          all the important tokens
+fn tokenize<'a>(exp: &'a str) -> Vec<&'a str> {
     let re = Regex::new(r" +|\n+").unwrap();
     re.split(exp).collect()
 }
 
-fn parse(exp: Vec<&str>) -> Exp {
-    /* regex definitions */
-    let var_name_re = Regex::new(r":^[a-z_]\\w*$").unwrap();
-    let sym_re = Regex::new("\"^[\\w\\-. ]+$\"").unwrap();
+fn parse(raw_code: &str) -> Vec<Exp> {
+    let mut exps = Vec::new();
 
-    Exp::Empty {}
+    for line in raw_code.split(';') { // TODO handle strings
+        let ts = tokenize(line);
+        exps.push(parse_line(&mut ts.iter().peekable()));
+    }
+    exps
 }
 
-fn expression(tokens: &mut Vec<&str>) {
-
+fn parse_line<'a>(ts: &mut Tokens<'a>) -> Exp {
+    expr(ts)
 }
 
-fn uni_op(tokens: &mut Vec<&str>) {
-
+fn expr<'a>(ts: &mut Tokens<'a>) -> Exp {
+    equality(ts)
 }
 
-fn bin_op(tokens: &mut Vec<&str>) {
+fn equality<'a>(ts: &mut Tokens<'a>) -> Exp {
+    let mut e = comparison(ts);
 
+    // this is not so idiomaitc, but rust's while let patterns are not expressive enough :(
+    loop {
+        let op = ts.peek().unwrap().to_string();
+        match op.as_ref() {
+            "==" => e = Exp::Binary {
+                operator: BinaryOp::Eq,
+                left: Box::new(e),
+                right: Box::new(comparison(ts))
+            },
+            "!=" => e = Exp::Binary {
+                operator: BinaryOp::Neq,
+                left: Box::new(e),
+                right: Box::new(comparison(ts))
+            },
+            _ => break,
+        };
+    }
+    e
 }
 
-fn def(tokens: &mut Vec<&str>) {
+fn comparison<'a>(ts: &mut Tokens<'a>) -> Exp {
+    let mut e = term(ts);
+    loop {
+        let op = ts.peek().unwrap().to_string();
+        match op.as_ref() {
+            ">" => e = Exp::Binary {
+                operator: BinaryOp::Gt,
+                left: Box::new(e),
+                right: Box::new(term(ts))
+            },
+            "<" => e = Exp::Binary {
+                operator: BinaryOp::Lt,
+                left: Box::new(e),
+                right: Box::new(term(ts))
+            },
+            ">=" => e = Exp::Binary {
+                operator: BinaryOp::Gte,
+                left: Box::new(e),
+                right: Box::new(term(ts))
+            },
+            "<=" => e = Exp::Binary {
+                operator: BinaryOp::Lte,
+                left: Box::new(e),
+                right: Box::new(term(ts))
+            },
+            _ => break,
+        };
+    }
+    e
+}
 
+fn term<'a>(ts: &mut Tokens<'a>) -> Exp {
+  let mut e = factor(ts);
+
+  loop {
+      let op = ts.peek().unwrap().to_string();
+      match op.as_ref() {
+          "-" => e = Exp::Binary {
+              operator: BinaryOp::Sub,
+              left: Box::new(e),
+              right: Box::new(term(ts))
+          },
+          "+" => e = Exp::Binary {
+              operator: BinaryOp::Add,
+              left: Box::new(e),
+              right: Box::new(term(ts))
+          },
+          _ => break,
+      };
+  }
+
+  e
+}
+
+fn factor<'a>(ts: &mut Tokens<'a>) -> Exp {
+  let mut e = unary(ts);
+
+  loop {
+      let op = ts.peek().unwrap().to_string();
+      match op.as_ref() {
+          "/" => e = Exp::Binary {
+              operator: BinaryOp::Div,
+              left: Box::new(e),
+              right: Box::new(term(ts))
+          },
+          "*" => e = Exp::Binary {
+              operator: BinaryOp::Mult,
+              left: Box::new(e),
+              right: Box::new(term(ts))
+          },
+          "%" => e = Exp::Binary {
+              operator: BinaryOp::Mod,
+              left: Box::new(e),
+              right: Box::new(term(ts))
+          },
+          _ => break,
+      };
+  }
+
+  e
+}
+
+fn unary<'a>(ts: &mut Tokens<'a>) -> Exp {
+  let op = ts.peek().unwrap().to_string();
+  match op.as_ref() {
+      "!" => {
+          let right = unary(ts);
+          Exp::Unary {
+              operator: UnaryOp::Inverse,
+              operand: Box::new(right)
+          }
+      },
+      "-" => {
+          let right = unary(ts);
+          Exp::Unary {
+              operator: UnaryOp::Negate,
+              operand: Box::new(right)
+          }
+      },
+      _ => primary(ts)
+  }
+}
+
+fn primary<'a>(ts: &mut Tokens<'a>) -> Exp {
+    let num_re = Regex::new(r"-?\d\d\d\d\d\d\d\d").unwrap();
+    let sym_re = Regex::new("\"[.]\"").unwrap();
+    let next = ts.peek().unwrap().to_string();
+
+    match next.as_ref() {
+        "false" => Exp::Literal { value: Val::Bool { value: false } },
+        "true" => Exp::Literal { value: Val::Bool { value: true } },
+        "nil" => Exp::Literal { value: Val::Nil },
+        "(" => {
+            let e = expr(ts);
+            let close_paren = ts.peek().unwrap().to_string();
+            if close_paren != ")".to_string() {
+                panic!("error: missing right parenthesis");
+            }
+            e
+        },
+        _ => {
+            if num_re.is_match(&next) {
+                Exp::Literal { value: Val::Num { value: i64::from_str(&next).unwrap() } }
+            } else if sym_re.is_match(&next) {
+                Exp::Literal { value: Val::Sym { value: next }}
+            } else {
+                panic!("syntax error");
+            }
+        }
+    }
 }
 
 /*********************************** evaluator ***************************************************/
-
+/*
 fn eval_exps(exps: Vec<Exp>, env: &mut Env) {
     for e in exps {
         eval(e, env);
@@ -90,7 +238,7 @@ fn eval_exps(exps: Vec<Exp>, env: &mut Env) {
 }
 
 fn eval(e: Exp, env: &mut Env) -> Val {
-    match e {
+/*    match e {
         Exp::Literal { value } => value,
         Exp::VarName { name } => {
             match env.get(&name) {
@@ -108,9 +256,10 @@ fn eval(e: Exp, env: &mut Env) -> Val {
         },
         Exp::Command { cmd, args } => Val::Nil {}, /* replace with command exec */
         Exp::Empty {} => Val::Nil {},
-    }
+    }*/
+    Val::Nil {}
 }
-
+*/
 /*************************************************************************************************/
 
 fn main() {
@@ -128,9 +277,8 @@ fn main() {
         match io::stdin().read_line(&mut code) {
             Ok(_n) => {
                 code.pop(); // remove newline
-                let exps = parse_code(&code);
-                //type::type(&exps);
-                eval_exps(exps, &mut env);
+                let exps = parse(&code);
+                //eval_exps(exps, &mut env);
             }
             Err(error) => println!("error: {}", error),
         }
